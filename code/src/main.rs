951aee0b33d189dd4b0fb5b5e90cf5677a0aa5d6
s194042 <hhhermansen@gmail.com>
2024-11-01 13:17:21 +0100

    bascily done

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,6 +1,7 @@
 use fastset::{set, Set};
+use outils::graph;
 use outils::prelude::DynamicConnectivity;
-use outils::types::{Edge, Edges};
+use outils::types::{Edge, Edges, EmptyWeight};
 use std::vec::Vec;
 use std::{cmp::max, io};
 use union_find::{QuickFindUf, QuickUnionUf, UnionBySize, UnionFind};
@@ -26,27 +27,28 @@ fn read_line_of_ints(input: &mut String) -> Vec<usize> {
 }
 
 fn minium_spanning_tree(
-    edges: &Vec<(usize, usize, usize, usize)>,
+    edges_with_order: &Vec<(usize,usize,usize,usize)>,
+    sorted_edges: &Vec<(usize, usize, usize, usize)>,
     n : usize,
 ) -> Result<(Vec<usize>, usize, usize, usize, usize), String> {
-    let m = edges.len()-1;
+    let m = sorted_edges.len()-1;
     let mut edges_in_st = Vec::new();
 
     let mut u = QuickUnionUf::<UnionBySize>::new(n+1);
     let mut max_tree_edge = 0;
     let mut max_mirror_edge = 0;
 
-    for (x, y, _, i) in &edges[1..m+1] {
+    for (x, y, _, i) in &sorted_edges[1..m+1] {
         if u.find(*x) != u.find(*y) {
             u.union(*x, *y);
             edges_in_st.push(*i);
     
-            max_tree_edge = max(edges[*i].3,max_tree_edge);
-            max_mirror_edge = max(edges[*i].3,max_mirror_edge);
+            max_tree_edge = max(edges_with_order[*i].3,max_tree_edge);
+            max_mirror_edge = max(edges_with_order[m+1-i].3,max_mirror_edge);
             
             if edges_in_st.len() == n - 1 {
-                let minimum_st_sum: usize = edges_in_st.iter().map(|x| edges[*x ].2).sum();
-                let mirror_sum: usize = edges_in_st.iter().map(|x| edges[m +1 - *x].2).sum();
+                let minimum_st_sum: usize = edges_in_st.iter().map(|x| edges_with_order[*x ].2).sum();
+                let mirror_sum: usize = edges_in_st.iter().map(|x| edges_with_order[m +1 - *x].2).sum();
                 return Ok((edges_in_st, minimum_st_sum, mirror_sum, max_tree_edge, max_mirror_edge));
             }
         }
@@ -55,25 +57,28 @@ fn minium_spanning_tree(
     Err("No spanning tree".to_string())
 }
 
-fn find_locked_edges(graph: &mut Vec<Vec<usize>>, degrees: &mut Vec<usize>, edges: &Vec<(usize, usize, usize, usize)>,) -> Set{
+fn find_locked_edges(edges: &Vec<(usize, usize, usize, usize)>, n : usize) -> Set{
     let mut locked_edges = Set::new(edges.len()-1);
-   
+    let mut graph : DynamicGraph<EmptyWeight> = DynamicGraph::new(n+1, 100);
+    let vertices : Vec<VertexIndex> = (0..n+1).map(|x| {VertexIndex(x)}).collect();
+    let mut edges_in_graph = vec![];
 
-    loop{
-        let edge_set_size_before = locked_edges.len();
 
-        for i in 1..edges.len(){
-            if degrees[edges[i].0] == 1 || degrees[edges[i].1] == 1{
-                locked_edges.insert(i);
-                degrees[edges[i].0] -= 1;
-                degrees[edges[i].1] -= 1;
-            }
-        }
-
-        if edge_set_size_before == locked_edges.len(){
-            return locked_edges;
-        }
+    for (v,w,_,_) in &edges[1..edges.len()]{
+        edges_in_graph.push(graph.insert_edge(vertices[*v], vertices[*w]).unwrap());
     }
+    println!("{:?}",edges_in_graph.len());
+
+    for i in 0..edges_in_graph.len(){
+        graph.delete_edge(edges_in_graph[i]);
+        if !graph.is_connected(vertices[edges[i+1].0], vertices[edges[i+1].1]){
+            locked_edges.insert(i+1);
+        }
+        graph.insert_edge(vertices[edges[i+1].0], vertices[edges[i+1].1]);
+    }
+
+    return locked_edges;
+
 }
 
 fn brute_force(
@@ -119,9 +124,8 @@ fn brute_force(
 
 
 fn minimum_mirror_spanning_tree(
-    graph: &mut Vec<Vec<usize>>,
-    degrees: &mut Vec<usize>,
     edges: &Vec<(usize, usize, usize, usize)>,
+    n: usize,
 ) -> Result<(usize, Set), String> {
     let mut edges_sorted_by_weight = edges.clone();
     let mut edges_sorted_by_mirror_weight = edges.clone();
@@ -131,7 +135,7 @@ fn minimum_mirror_spanning_tree(
     // edges_sorted_by_mirror_weight
     //     .sort_by(|edge_i, edge_j| edges[edge_i.3 - 1].2.cmp(&edges[edge_j.3 - 1].2));
 
-    let (spanning_tree, tree_weight, mirror_weight,max_tree_edge,max_mirror_edge) = match minium_spanning_tree(&edges_sorted_by_weight,graph.len()-1) {
+    let (spanning_tree, tree_weight, mirror_weight,max_tree_edge,max_mirror_edge) = match minium_spanning_tree(edges, &edges_sorted_by_weight,n) {
         Err(s) => return Err(s),
         Ok((spanning_tree, spanning_tree_weigth, mirror_weigth,max_tree_edge, max_mirror_edge)) => {
             (spanning_tree, spanning_tree_weigth, mirror_weigth,max_tree_edge, max_mirror_edge)
@@ -140,21 +144,24 @@ fn minimum_mirror_spanning_tree(
 
     println!("{:?} {:?} {:?}",tree_weight,mirror_weight,spanning_tree);
     let mut blacklisted_edges = (0..edges.len()).map(|_| false).collect::<Vec<bool>>();
-    let mut locked_edges = find_locked_edges(graph, degrees, edges);
+    let mut locked_edges = find_locked_edges(edges,n);
+    let mut graph = DynamicGraph::new(n+1,100);
 
     for index in &locked_edges{
         blacklisted_edges[*index] = true;
+        graph.insert_edge(VertexIndex(edges[*index].0), VertexIndex(edges[*index].1));
+        
     }
 
     println!("Locked edges (edge idx): {:?}",locked_edges);
 
     Ok(brute_force(
         edges,
-        &mut DynamicGraph::new(graph.len(),100),
+        &mut graph,
         &mut locked_edges,
         &blacklisted_edges,
         1,
-        graph.len()-1,
+        n,
     ))
 }
 
@@ -179,19 +186,19 @@ fn main() {
     
     //let sample = vec![15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 25, 28, 29, 30, 31, 32, 33, 34, 35, 38];
 
-    let sample = vec![7, 5, 32, 20, 30, 33, 31, 25, 14, 8, 11, 38, 35, 19, 1, 3, 6, 17, 36, 37, 28, 18, 2, 15, 26, 29, 10, 23, 13];
+    // let sample = vec![7, 5, 32, 20, 30, 33, 31, 25, 14, 8, 11, 38, 35, 19, 1, 3, 6, 17, 36, 37, 28, 18, 2, 15, 26, 29, 10, 23, 13];
 
-    let mut sample_sum = 0;
+    // let mut sample_sum = 0;
 
-    for index in sample{
-        sample_sum += edges_with_order[index].2;
-    }
+    // for index in sample{
+    //     sample_sum += edges_with_order[index].2;
+    // }
 
-    println!("This is the sample sum: {:?}",sample_sum);
+    // println!("This is the sample sum: {:?}",sample_sum);
 
     let mut sorted_edges = edges_with_order.clone();
     sorted_edges.sort_by(|e1, e2| e1.2.cmp(&e2.2));
 
-    println!("{:?}", minimum_mirror_spanning_tree(&mut graph, &mut degrees, &edges_with_order));
+    println!("{:?}", minimum_mirror_spanning_tree(&edges_with_order,n));
 
 }
